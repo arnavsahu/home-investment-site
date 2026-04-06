5805dab713cee09d673713e7870bf1fc7337d0b7
Arnav Sahu <arnavsahu@Arnavs-MacBook-Pro.local>
2026-04-06 13:23:39 -0700

    update tp App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,16 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Import routing components
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Model from './Pages/Model';  // Import the Model component
+import Model from "./Pages/Model";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/model" element={<Model />} />
+        <Route path="/" element={<Model />} />
       </Routes>
     </Router>
-  );
-}
-
-function Home() {
-  return (
-    <div>
-      <h1>Welcome to the Home Page</h1>
-    </div>
   );
 }
 
